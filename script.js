a2f1633f77054ae667a37b2a7aa51056583b1cdd
ellemari0129 <marielletimajo.apay@ph.ncs-i.com>
2024-05-24 11:05:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 
-for (var i = 1; i <= 10; i++) {
+for (var i = 1; i <= 4; i++) {
   var thumb = document.createElement("img");
   thumb.src = "images/image" + i + ".jpg";
   thumb.alt = "Image " + i;
